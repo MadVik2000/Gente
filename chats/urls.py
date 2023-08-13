49ad0f9205ee0c45c4b682b5fdbb4d701cf71392
MadVik2000 <vkhtgupta@gmail.com>
2023-08-13 21:27:39 +0530

    renamed api modules

--- chats/urls.py
+++ chats/urls.py
@@ -4,17 +4,17 @@ This file contains all the API endpoints for users application.
 
 from django.urls import path
 
-from chats.api import create_chat_session_message, start_chat
+from chats.api import chat_session, chat_session_message
 
 urlpatterns = [
     path(
         "start/",
-        start_chat.StartChatAPI.as_view(),
+        chat_session.StartChatAPI.as_view(),
         name="start-chat",
     ),
     path(
         "message/",
-        create_chat_session_message.CreateChatSessionMessageAPI.as_view(),
+        chat_session_message.CreateChatSessionMessageAPI.as_view(),
         name="send-message",
     ),
 ]
