0eb390e511adc8636b5418417823bf67ca3d56b1
MadVik2000 <vkhtgupta@gmail.com>
2023-08-13 22:14:16 +0530

    added created_by updated_by and deleted_by in custom model

--- helpers/models.py
+++ helpers/models.py
@@ -2,9 +2,12 @@
 This file contains all the custom models used in the project
 """
 
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from helpers.mixins import ModelDifferenceMixin
+
+User = get_user_model()
 
 
 class CustomModel(models.Model, ModelDifferenceMixin):
@@ -15,6 +18,16 @@ class CustomModel(models.Model, ModelDifferenceMixin):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, null=True, blank=True, related_name="%(class)s_created_by"
+    )
+    updated_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, null=True, blank=True, related_name="%(class)s_updated_by"
+    )
+    deleted_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, null=True, blank=True, related_name="%(class)s_deleted_by"
+    )
 
     def save(self, *args, **kwargs):
         skip_cleaning = kwargs.pop("skip_cleaning", False)
