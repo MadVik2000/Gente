ccb89f35ee5a3c539b3d4cd63f6fdf9fc3f7dd4c
MadVik2000 <vkhtgupta@gmail.com>
2023-08-13 21:05:16 +0530

    moved middlewares module from users app to helpers app, added JWTAuthentication, removed CustomAuthenticationMiddleware

---
+++ helpers/middleware.py
@@ -0,0 +1,53 @@
+"""
+This file contains all middlewares related to users application.
+"""
+import jwt
+from django.contrib.auth import get_user_model
+from jwt.exceptions import (
+    ExpiredSignatureError,
+    InvalidSignatureError,
+    InvalidTokenError,
+)
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+User = get_user_model()
+
+
+class JWTAuthentication(BaseAuthentication):
+    """
+    Backend authentication class for JWT based auth.
+    """
+
+    def authenticate(self, request):
+        auth = request.META.get("HTTP_AUTHORIZATION", "").split()
+        if not auth or auth[0].lower() != "bearer":
+            return None
+
+        if len(auth) == 1:
+            raise AuthenticationFailed(
+                "Invalid token header. No credentials provided."
+            )
+        elif len(auth) > 2:
+            raise AuthenticationFailed(
+                "Invalid token header. Token string should not contain spaces."
+            )
+        private_key = open("jwtRS256.key.pub").read().encode()
+        try:
+            data = jwt.decode(
+                auth[1],
+                private_key,
+                algorithms=[
+                    "RS256",
+                ],
+            )
+            user = User._default_manager.get(uuid=data["user_uuid"])
+        except (
+            ExpiredSignatureError,
+            InvalidSignatureError,
+            InvalidTokenError,
+            User.DoesNotExist,
+        ) as _error:
+            pass
+
+        return (user, None)
