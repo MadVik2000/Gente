ffcbc2cdfc4de5b04ec6a523cf9d1e2b6a7da7ce
MadVik2000 <vkhtgupta@gmail.com>
2023-08-19 20:50:15 +0530

    added hashmap overriding methods in custom redis caching

--- helpers/caching.py
+++ helpers/caching.py
@@ -3,10 +3,11 @@ This file contains custom caching modules used throughout the project
 """
 
 from collections.abc import Iterable
-from typing import Union
+from typing import Any, Optional, Union
 
 from django.core.cache.backends.redis import RedisSerializer
 from redis.client import Redis
+from redis.exceptions import DataError
 
 
 class CustomRedisCaching(Redis):
@@ -23,7 +24,9 @@ class CustomRedisCaching(Redis):
         values = [self.value_serializer.dumps(value) for value in values]
         return super().lpush(key, *values)
 
-    def lrange(self, key: str, start: int = 0, end: int = -1, instance: bool = True) -> list:
+    def lrange(
+        self, key: str, start: int = 0, end: int = -1, instance: Optional[bool] = True
+    ) -> list:
         """
         Customised redis LRANGE command to retrieve django model instances
         """
@@ -37,7 +40,7 @@ class CustomRedisCaching(Redis):
         values = [self.value_serializer.dumps(value) for value in values]
         return super().rpush(key, *values)
 
-    def lpop(self, name, count: Union[int, None] = None) -> list:
+    def lpop(self, name: str, count: Union[int, None] = None) -> list:
         values = list(super().lpop(name, count))
         if values:
             return (
@@ -48,8 +51,31 @@ class CustomRedisCaching(Redis):
         else:
             return []
 
+    def hset(
+        self,
+        name: str,
+        key: Optional[str] = None,
+        value: Optional[Any] = None,
+        mapping: Optional[dict[str, Any]] = None,
+        items: Optional[list[Any]] = [],
+    ) -> int:
+        if key:
+            if value is None:
+                raise DataError("'hset' with given key but no value to it")
+            items.extend([key, self.value_serializer.dumps(value)])
+
+        if mapping:
+            for mapping_key, mapping_value in mapping.items():
+                items.extend([mapping_key, self.value_serializer.dumps(mapping_value)])
+
+        return super().hset(name=name, items=items)
+
+    def hget(self, name: str, key: str) -> Any:
+        value = super().hget(name, key)
+        return self.value_serializer.loads(value) if value else None
+
     def hgetall(self, name: str) -> dict:
         return {
-            field.decode("utf-8"): value.decode("utf-8")
+            field.decode("utf-8"): self.value_serializer.loads(value)
             for field, value in super().hgetall(name).items()
         }
